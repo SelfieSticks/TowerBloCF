ae2f831eb836c2f557cf1edebd9ce187a02d8372
Harjuno Perwironegoro <hperwironegoro@gmail.com>
2017-06-24 15:23:03 +0100

    Implemented Camera and Dropper vertical movement.

--- Assets/Scripts/BlockFunctionality/Tower.cs
+++ Assets/Scripts/BlockFunctionality/Tower.cs
@@ -8,6 +8,9 @@ namespace Assets.Scripts {
 
     public class Tower {
 
+        public delegate void LandingUpdate(Vector3 landingPosition);
+        public LandingUpdate landingHandler = DebugLanding;
+
         public static Tower singlePlayerTower;//only for test purposes to be accessable due to not selected way how this will be accessed
         public GameObject TopBlock { get; private set; }
         private float distanceBetweenBlocks; // distance that is considered as between blocks
@@ -28,18 +31,22 @@ namespace Assets.Scripts {
             // checks vertically
             if (dist < distanceBetweenBlocks + 0.1f && dist > 0)
             {
-                float horizontalDist = Mathf.Abs(newBlock.transform.position.x - TopBlock.transform.position.y);
+                float horizontalDist = Mathf.Abs(newBlock.transform.position.x - TopBlock.transform.position.x);
                 if (horizontalDist < blockWidth)
                 {
                     TopBlock = newBlock;
                     blocklevels++;
-                    Debug.Log("new block on top");
+                    landingHandler(newBlock.transform.position);
                     return;
                 }
             }
             // if it is plotted properly it should not reach this code
             // so we delete the object
             GameObject.Destroy(newBlock);
+        }
+
+        private static void DebugLanding(Vector3 landingPosition) {
+            Debug.Log("New block on top at: " + landingPosition);
         }
     }
 }
---
+++ Assets/Scripts/Landing/LandingOffset.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts;
+
+public class LandingOffset : MonoBehaviour {
+    [Range(0f, 1f)] public float lerpRate;
+
+    private Vector3 cameraOffset;
+    private Vector3 destPos;
+
+    void Start () {
+        Tower.singlePlayerTower.landingHandler += Move;
+        // Keep the camera offset set in the Scene.
+        cameraOffset = transform.position;
+        destPos = cameraOffset;
+	}
+
+    void Update() {
+        transform.position = Vector3.Lerp(transform.position, destPos, lerpRate);
+    }
+	
+	private void Move (Vector3 landingPosition) {
+        Vector3 landingY = new Vector3(0, landingPosition.y, 0);
+        destPos = landingY + cameraOffset;
+	}
+}
