add83373d5bbfcec6e92d40032dde4bd15b34383
nick96st <nikolay.yotov@abv.bg>
2017-06-25 15:52:29 +0100

    goldenBlock access to material

--- Assets/Scripts/GameControllers/GameManager.cs
+++ Assets/Scripts/GameControllers/GameManager.cs
@@ -10,12 +10,16 @@ namespace Assets.Scripts {
         Tower player1Tower;
         Tower player2Tower;// will be used only in multiplayer
         GameObject startPlatform1;
+        public Material goldenBoxLocal;
+        public static Material goldenBoxMaterial;
+
         private void Awake()
         {
             // sets player on platform to the base platform
             startPlatform1 = GameObject.Find("base");
             // creates a new tower manager 
-            player1Tower = new Tower(startPlatform1,100,10);
+            player1Tower = new Tower(startPlatform1,100,5);
+            goldenBoxMaterial = goldenBoxLocal;
         }
     }
 }
