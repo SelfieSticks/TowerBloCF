877cb66004a345360e2fe141a248ff4aaf416e77
nick96st <nikolay.yotov@abv.bg>
2017-06-25 15:53:25 +0100

    Cooldown on dropping block

--- Assets/Scripts/PlayerControllers/getPos.cs
+++ Assets/Scripts/PlayerControllers/getPos.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class getPos : MonoBehaviour {
 
 	public GameObject blockPrefab;
+    [SerializeField]
+    private float cooldown = 1.5f;
+    private bool isOnCooldown = false;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
+    IEnumerator cooldownToDrop()
+    {
+        yield return new WaitForSeconds(cooldown);
+        isOnCooldown = false;
+    }
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown ("Fire1")) {
+		if (Input.GetButtonDown ("Fire1") && !isOnCooldown) {
 
 			Instantiate (blockPrefab).transform.position = this.transform.position;
+            isOnCooldown = true;
+            StartCoroutine("cooldownToDrop");
 
 		}
 	}
