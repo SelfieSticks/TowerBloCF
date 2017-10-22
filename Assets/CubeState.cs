95aa4d55e3187579dc68e5194ab23d952a29adb1
Tencho Tenev <tenev.dev@outlook.com>
2017-10-22 16:58:56 +0100

    Add camera control and cube states

---
+++ Assets/CubeState.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeState : MonoBehaviour {
+
+    [SerializeField] private Material normal;
+    [SerializeField] private Material lit;
+    [SerializeField] private Material gold;
+
+    [SerializeField] private float goldAccuracy = .05f;
+    [SerializeField] private float litAccuracy = .15f;
+
+    private Renderer renderer;
+
+	// Use this for initialization
+	void Start () {
+        renderer = GetComponent<Renderer>();
+        renderer.material = normal;
+	}
+	
+    private void LightOn()
+    {
+        renderer.material = lit;
+    }
+
+    private void Gold()
+    {
+        renderer.material = gold;
+    }
+
+    internal void Set(float acc)
+    {
+        if (acc <= goldAccuracy)
+        {
+            Gold();
+        } else if(acc <= litAccuracy)
+        {
+            LightOn();
+        }
+    }
+}
---
+++ Assets/Scripts/Orbit.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float cameraDistance = 10;
+    [SerializeField] private float rotationSpeed = 10;
+    [SerializeField] private float verticalOffsetFromTarget = 4;
+
+    private Vector3 cameraRotation;
+    private float cameraAngle;
+
+    private void Start()
+    {
+        cameraRotation = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        cameraAngle += rotationSpeed * Time.deltaTime;
+        cameraAngle %= 2 * Mathf.PI;
+
+        cameraRotation.x = cameraDistance * Mathf.Sin(cameraAngle);
+        cameraRotation.z = cameraDistance * Mathf.Cos(cameraAngle);
+
+        cameraRotation.y = Mathf.Lerp(cameraRotation.y, target.position.y + verticalOffsetFromTarget, .1f);
+    }
+    
+    private void LateUpdate()
+    {
+        SetCameraTransform();
+    }
+
+    // Update the position and orientation of the camera to match the cameraRotation and cameraDistance
+    private void SetCameraTransform()
+    {
+        // Set the position of the camera based on the desired rotation towards and distance from the Player model
+        transform.localPosition = cameraRotation;
+
+        // Set the camera to look towards the Player model
+        transform.LookAt(target);
+    }
+}
