26b0d76de54c6fee5c6a7f3270cd8da43020a36d
Perwironegoro <hp1515@ic.ac.uk>
2017-11-06 00:36:30 +0000

    Fully implemented height tracking for achievements, and score tracking.

---
+++ Assets/Scripts/GameControllers/BlockEventBus.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BlockType 
+{
+    NORMAL,
+    LIT,
+    GOLD,
+    DIAMOND
+}
+
+public class BlockEventBus : MonoBehaviour 
+{
+    public delegate void Block(CubeState state);
+    public event Block BlockLand;
+
+	public void OnBlockLand(CubeState state) 
+    {
+        BlockLand(state);
+    }
+}
--- Assets/Scripts/GameControllers/ScoreManager.cs
+++ Assets/Scripts/GameControllers/ScoreManager.cs
@@ -1,39 +1,75 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
-public class ScoreManager : MonoBehaviour {
+[RequireComponent(typeof(BlockEventBus))]
+public class ScoreManager : MonoBehaviour 
+{
+    [SerializeField] private Text scoreText;
+
     // 4 types of blocks - normal, lit, gold, diamond
-    // Diamond blocks multiply x2 for 'multiplierTime'
+    // Diamond blocks multiply score by x2 for 10s
     public int Score { get; private set; }
-    public int Height { get; private set; }
-    [SerializeField] private int diamondMultiplier;
-    [SerializeField] private float multiplierTime = 10f;
+    [SerializeField] private int diamondMultiplier = 2;
+    [SerializeField] private float multiplierDuration = 10f;
+    private float multiplierTimer = 0;
 
-    [SerializeField] private int[] cubeScores = { 1, 3, 5, 10 };
+    [SerializeField] private int normalScore = 1;
+    [SerializeField] private int litScore = 3;
+    [SerializeField] private int goldScore = 5;
+    [SerializeField] private int diamondScore = 10;
+    private int[] cubeScores;
 
-    void Update() {
-        multiplierTime -= Time.deltaTime;
-        if (multiplierTime < 0f) {
-            multiplierTime = 0f;
-        }
+    private BlockType lastBlockType;
+
+    private void Awake() 
+    {
+        cubeScores = new int[]{ normalScore, litScore, goldScore, diamondScore };
+        GetComponent<BlockEventBus>().BlockLand += ScoreBlock;
     }
 
-    public enum CubeType {
-        NORMAL,
-        LIT,
-        GOLD,
-        DIAMOND
+    void Update() 
+    {
+        multiplierTimer -= Time.deltaTime;
+        if (multiplierTimer < 0f)
+            multiplierTimer = 0f;
     }
-    public void ScoreCube(CubeType landType) {
-        Height++;
 
-        int cubeScore = cubeScores[(int)landType];
-        if (landType == CubeType.DIAMOND) {
-            multiplierTime = 10f;
+    private void ScoreBlock(CubeState state) 
+    {
+        BlockType blockType = state.CubeType;
+
+        // Scoring
+        int cubeScore;
+        switch(lastBlockType) 
+        {
+            case BlockType.GOLD:
+                if(blockType == BlockType.GOLD) 
+                {
+                    cubeScore = 20;
+                    break;
+                }
+                goto default;
+            default:
+                cubeScore = cubeScores[(int)blockType];
+                break;
         }
 
-        int multiplier = multiplierTime == 0 ? diamondMultiplier : 1;
+        if (blockType == BlockType.DIAMOND) 
+        {
+            multiplierTimer = multiplierDuration;
+        }
+
+        int multiplier = multiplierTimer == 0 ? 1 : diamondMultiplier;
         Score += cubeScore * multiplier;
+        lastBlockType = blockType;
+
+        if(scoreText) {
+            scoreText.text = "Score: " + Score;
+        }
+    }
+
+    private void OnDestroy() 
+    {
+        GetComponent<BlockEventBus>().BlockLand -= ScoreBlock;
     }
 }
