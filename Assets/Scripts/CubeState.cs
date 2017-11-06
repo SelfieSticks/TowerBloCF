c4105d637629a3f3f24d06fd1d9d5927560d7c4e
Perwironegoro <hp1515@ic.ac.uk>
2017-11-06 00:42:36 +0000

    Moved to scripts folder, added a CubeType to track the block type.

---
+++ Assets/Scripts/CubeState.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeState : MonoBehaviour {
+
+    [SerializeField] private Material normal;
+    [SerializeField] private Material lit;
+    [SerializeField] private Material gold;
+
+    [SerializeField] private float goldAccuracy = .05f;
+    [SerializeField] private float litAccuracy = .15f;
+
+    [SerializeField] private AudioClip normalSound;
+    [SerializeField] private AudioClip goldSound;
+
+    public BlockType CubeType { get; private set; }
+
+    private AudioSource audioSource;
+    private new Renderer renderer;
+
+	// Use this for initialization
+	void Start () {
+        renderer = GetComponent<Renderer>();
+        renderer.material = normal;
+        CubeType = BlockType.NORMAL;
+
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = normalSound;
+	}
+	
+    private void LightOn()
+    {
+        CubeType = BlockType.LIT;
+        renderer.material = lit;
+    }
+
+    private void Gold()
+    {
+        CubeType = BlockType.GOLD;
+        renderer.material = gold;
+        audioSource.clip = goldSound;
+    }
+
+    internal void Set(float acc)
+    {
+        if (acc <= goldAccuracy)
+        {
+            Gold();
+        } else if(acc <= litAccuracy)
+        {
+            LightOn();
+        }
+        audioSource.Play();
+    }
+}
