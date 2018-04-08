625cbdc440c80e3c85d659a8c74f4bbb3a36bc2a
hp1515@ic.ac.uk <unconfigured@null.spigotmc.org>
2018-04-08 15:42:35 +0100

    Added further options for the following script.

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Follow : MonoBehaviour {
+    [SerializeField] private GameObject target;
+    [SerializeField] private float lerpRate = 0.25f;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private bool lockX;
+    [SerializeField] private bool lockY;
+    [SerializeField] private bool lockZ;
+
+    private Vector3 targetPos;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (target)
+        {
+            targetPos = target.transform.position + offset;
+            if(lockZ) {
+                targetPos.z = transform.position.z;
+            }
+            if(lockY) {
+                targetPos.y = transform.position.y;
+            }
+            if(lockX) {
+                targetPos.x = transform.position.x;
+            }
+            transform.position = Vector3.Lerp(transform.position, targetPos, lerpRate);
+        }
+
+    }
+}
