f64097ece9c9719959da5c14abde19e04973319c
Perwironegoro <hp1515@ic.ac.uk>
2018-04-08 17:23:01 +0100

    Renamed file, repurposed for general block event handling.

---
+++ Assets/Scripts/GameControllers/StatTrackers/BlockEventTarget.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class BlockEventTarget : MonoBehaviour 
+{
+    protected virtual void Awake() 
+    {
+        var be = GameObject.FindObjectOfType<BlockEvents>();
+        be.BlockLand += OnBlockLand;
+        be.PostBlockUpdate += OnBlockEvent;
+    }
+
+    protected virtual void OnBlockLand(CubeState state) {}
+    protected virtual void OnBlockEvent() {}
+
+    protected virtual void OnDestroy() 
+    {
+        var beb = GameObject.FindObjectOfType<BlockEvents>();
+        if(!beb) 
+        {
+            return;
+        }
+
+        beb.BlockLand -= OnBlockLand;
+        beb.PostBlockUpdate -= OnBlockEvent;
+    }
+}
