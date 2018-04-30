8ea220420e791c918b646d34330d8bbf21054809
Perwironegoro <hp1515@ic.ac.uk>
2018-04-30 21:29:29 +0100

    Organised components into appropriate gameobjects. Added highscore tracking and game ending functionality.

---
+++ Assets/Scripts/Game Logic/Level/Level.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour {
+    [SerializeField] private ScoreTracker scoreTracker;
+    [SerializeField] private SceneSwitch sceneSwitch;
+    [SerializeField] private HighscoreBehaviour highscores;
+	
+	public void End() {
+        highscores.SaveHighscore(PlayerPrefs.GetString("Name"), scoreTracker.Score);
+        sceneSwitch.NextScene("Highscores");
+	}
+}
---
+++ Assets/Scripts/Game Logic/Stats/HighscoreBehaviour.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class HighscoreBehaviour : MonoBehaviour {
+    public abstract bool SaveHighscore(string name, int score);
+    public abstract List<KeyValuePair<string, int>> GetHighscores();
+}
