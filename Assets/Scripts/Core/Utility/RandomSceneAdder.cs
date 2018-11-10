3e1616156a86a7c11708e692bf6ea71d54de5b4b
Harjuno Perwironegoro <hperwironegoro@gmail.com>
2018-11-10 16:49:33 +0000

    Add scene switching.

---
+++ Assets/Scripts/Core/Utility/RandomSceneAdder.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RandomSceneAdder : MonoBehaviour
+{
+    [SerializeField] private string[] sceneNames;
+
+    public void AddScene()
+    {
+        int index = Random.Range(0, sceneNames.Length);
+        string sceneName = sceneNames[index];
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+    }
+}
