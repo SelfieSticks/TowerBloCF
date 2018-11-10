71454a589370a88229a0ef5195b4a8bd4195d290
Harjuno Perwironegoro <hperwironegoro@gmail.com>
2018-11-10 16:13:38 +0000

    Added Player Name to Settings.

---
+++ Assets/Scripts/Core/Utility/PlayerPrefs/PrefSaver.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PrefSaver : MonoBehaviour
+{
+    [SerializeField] private string PrefName;
+
+    public void Save(string value)
+    {
+        PlayerPrefs.SetString(PrefName, value);
+    }
+}
