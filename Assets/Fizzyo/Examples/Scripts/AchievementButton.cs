bd2c3f13788801bbdfd38dc55acb48b2cce310b7
SimonDarksideJ <darkside@zenithmoon.com>
2019-02-01 12:35:58 +0000

    Updated project to 2018.2.20 (latest) Updated Fizzyo Framework Resolved build issues

---
+++ Assets/Fizzyo/Examples/Scripts/AchievementButton.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Class for category button data
+/// </summary>
+public class AchievementButton : MonoBehaviour
+{
+    // Game Object that links to the area where the achievements are shown
+    public GameObject achList;
+
+    // Button states
+    public Sprite neutral, selected;
+
+    // Button sprite
+    private Image sprite;
+
+    void Awake()
+    {
+        sprite = GetComponent<Image>();
+    }
+
+    /// <summary>
+    /// Changes the sprite of a button and sets it active
+    /// </summary>
+    public void Click()
+    {
+        if (sprite.sprite == neutral)
+        {
+            sprite.sprite = selected;
+            achList.SetActive(true);
+        }
+        else
+        {
+            sprite.sprite = neutral;
+            achList.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Fizzyo/Examples/Scripts/FollowCameraController.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+
+public class FollowCameraController : MonoBehaviour
+{
+
+    public GameObject player;       //Public variable to store a reference to the player game object
+
+    private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+
+    // Use this for initialization
+    void Start()
+    {
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        Vector3 newCamPos = player.transform.position;
+        newCamPos.y = offset.y; //keep y the same
+
+        offset = transform.position - newCamPos;
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+
+        Vector3 newCamPos = player.transform.position;
+        newCamPos.y = offset.y; //keep y the same
+
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        transform.position = newCamPos + offset;
+    }
+}
