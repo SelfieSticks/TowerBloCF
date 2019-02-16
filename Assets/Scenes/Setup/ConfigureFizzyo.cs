2a4475c7b7a8689fa546fad3ec4a54ac9b170a03
Perwironegoro <hp1515@ic.ac.uk>
2019-02-16 20:07:55 +0000

    Added a setup scene to force into Settings.

--- Assets/Scenes/Setup/ConfigureFizzyo.cs
+++ Assets/Scenes/Setup/ConfigureFizzyo.cs
@@ -4,17 +4,28 @@ using UnityEngine.SceneManagement;
 
 public class ConfigureFizzyo : MonoBehaviour
 {
-    private const string SETUP_DATE_KEY = "setupDate";
-    
+    [SerializeField] public string settingsSceneName;
+    [SerializeField] public string menuSceneName;
+
+    private const string SETUP_DATE_KEY = "setup_date";
+    private const string DEFAULT_VALUE = "";
 
     void Start()
     {
-        var isFirstTime = PlayerPrefs.GetString(SETUP_DATE_KEY, null) == null;
+        var setupDateString = PlayerPrefs.GetString(SETUP_DATE_KEY, DEFAULT_VALUE);
+
+        var isFirstTime = setupDateString == DEFAULT_VALUE;
         if (isFirstTime)
         {
             var now = DateTime.UtcNow.ToString();
+            Debug.Log(setupDateString);
             PlayerPrefs.SetString(SETUP_DATE_KEY, now);
-
+            SceneManager.LoadScene(settingsSceneName);
+        }
+        else
+        {
+            Debug.Log(setupDateString);
+            SceneManager.LoadScene(menuSceneName);
         }
     }
 }
