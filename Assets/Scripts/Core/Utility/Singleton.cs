4350fea36b16df5cbc83ed43204a876d578d2d15
Perwironegoro <hp1515@ic.ac.uk>
2019-02-17 02:19:24 +0000

    Added post-singleton event.

--- Assets/Scripts/Core/Utility/Singleton.cs
+++ Assets/Scripts/Core/Utility/Singleton.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Unoper.Unity.Utils
 {
     public abstract class Singleton<T> : MonoBehaviour 
         where T : struct, IConvertible
     {
+        [SerializeField] private UnityEvent OnSingletonCreated = new UnityEvent();
+
         private static HashSet<T> _singletons = new HashSet<T>();
 
         [SerializeField] private T Id;
@@ -15,18 +18,23 @@ namespace Unoper.Unity.Utils
 
 	    private void Awake ()
         {
+            Debug.Log("Attempting to create singleton: " + Id.ToString());
             if (_singletons.Contains(Id))
             {
+                Debug.Log("Singleton already exists for: " + Id.ToString() + ", destroying this GameObject.");
                 Destroy(gameObject);
             } 
             else
             {
+                Debug.Log("Singleton created for: " + Id.ToString());
                 if(!DestroyOnLoad) {
                     DontDestroyOnLoad(gameObject);
                 }
 
                 _isSingleton = true;
                 _singletons.Add(Id);
+
+                OnSingletonCreated.Invoke();
             }
 	    }
 
