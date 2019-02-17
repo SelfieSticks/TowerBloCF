00a2491148069ccfdf236572ede089fcd8264f03
Perwironegoro <hp1515@ic.ac.uk>
2019-02-17 02:13:41 +0000

    Added script for creating Prefab at runtime.

---
+++ Assets/Scripts/Core/Utility/CreatePrefab.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePrefab : MonoBehaviour
+{
+    [SerializeField] private GameObject prefab;
+
+    public void Create()
+    {
+        Instantiate(prefab, Vector3.zero, Quaternion.identity);
+    }
+}
